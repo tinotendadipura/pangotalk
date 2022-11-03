c67452a3e84daa4698a1b4c157d4319e05b45bae
Tinotenda Dipura <tinotendadipura2@gmail.com>
2022-11-03 14:47:34 +0200

    commit

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pangotalk.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pangotalk.settings')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/

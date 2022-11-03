a5034c1ad37ed5c38f13c40cc61ad8ea754b8070
Tinotenda Dipura <tinotendadipura2@gmail.com>
2022-11-03 07:03:30 +0200

    COMMIT

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -144,10 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 
-)
 
 
 

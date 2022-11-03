493cca716e065e823fe332729ae3868bd5d38d3e
Tinotenda Dipura <tinotendadipura2@gmail.com>
2022-11-03 15:17:07 +0200

    commit

--- controlCentre/views.py
+++ controlCentre/views.py
@@ -85,8 +85,6 @@ def control_center_dashboard(request):
 @login_required(login_url='accounts/login' )
 @allowed_users(allowed_roles = ['Adminstrator'])
 def new_accounts(request):
-   
-    
     unverified_business = BusinessProfile.objects.filter(account_authorisation_status = False)
     verified_business   = BusinessProfile.objects.filter(account_authorisation_status = True).count()
     suspended_qaccounts = BusinessProfile.objects.filter(account_suspended_status     = True).count()

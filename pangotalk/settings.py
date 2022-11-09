eecc9c31fefc27e043170f55e92b89809d1b5460
Tinotenda Dipura <tinotendadipura2@gmail.com>
2022-11-09 23:51:46 +0200

    commit

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -198,6 +198,7 @@ EMAIL_PORT     = '465'
 AWS_ACCESS_KEY_ID = 'AKIA4S6RBKLVOTF5JZ77'
 AWS_SECRET_ACCESS_KEY = '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'
 DEFAULT_FROM_EMAIL   = 'Accounts<account@pangotalk.com>'
+APPLICATION_EMAIL    = 'Accounts<account@pangotalk.com>'
 AWS_DEFAULT_ACL = None
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

14b3e41c82c0346a77163b0175a449c72f1ab2a7
Tinotenda Dipura <tinotendadipura2@gmail.com>
2022-11-15 20:03:13 +0200

    commit

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -191,24 +191,24 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 
    
 
-EMAIL_BACKEND = 'django_ses.SESBackend'
-EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-EMAIL_PORT     = '465'
-AWS_ACCESS_KEY_ID = 'AKIA4S6RBKLVOTF5JZ77'
-AWS_SECRET_ACCESS_KEY = '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'
-DEFAULT_FROM_EMAIL   = 'Accounts<account@pangotalk.com>'
-APPLICATION_EMAIL    = 'Accounts<account@pangotalk.com>'
-AWS_DEFAULT_ACL = None
+# EMAIL_BACKEND = 'django_ses.SESBackend'
+# EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+# EMAIL_PORT     = '465'
+# AWS_ACCESS_KEY_ID = 'AKIA4S6RBKLVOTF5JZ77'
+# AWS_SECRET_ACCESS_KEY = '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'
+# DEFAULT_FROM_EMAIL   = 'Accounts<account@pangotalk.com>'
+# APPLICATION_EMAIL    = 'Accounts<account@pangotalk.com>'
+# AWS_DEFAULT_ACL = None
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST    = 'smtp.gmail.com'
-# EMAIL_PORT     = '587'
-# EMAIL_HOST_USER  = 'taita.edu@gmail.com'
-# EMAIL_HOST_PASSWORD  = 'ulzoeaxlyboudqnm'
-# APPLICATION_EMAIL    = 'PangoTalk<taita.edu@gmail.com>'
-# DEFAULT_FROM_EMAIL   = 'PangoTalk<taita.edu@gmail.com>'
-# EMAIL_USE_TLS  = True
-# EMAIL_USE_SSL   = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST    = 'smtppro.zoho.com'
+EMAIL_PORT     = '587'
+EMAIL_HOST_USER  = 'info@pangotalk.com'
+EMAIL_HOST_PASSWORD  = '@shinobi97'
+APPLICATION_EMAIL    = 'PangoTalk<info@pangotalk.com>'
+DEFAULT_FROM_EMAIL   = 'PangoTalk<info@pangotalk.com>'
+EMAIL_USE_TLS  = True
+EMAIL_USE_SSL   = True
 #    
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
